c5ce4fa6da7ab73ecdef5ca9f59e1e532b23e8fe
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2024-10-07 00:19:35 +0600

    practice data types

---
+++ first-day/DataTypeExamplePractice.java
@@ -0,0 +1,14 @@
+class DataTypeExamplePractice{
+    public static void main (String[] arg){
+        String studentName="junaid";
+        int studentId=01;
+        int studentAge=11;
+        float studentFee=100.10f;
+        char studentGrade='B';
+        int width=100;
+        int length=300;
+        int area=length*width;
+        System.out.println("Rectangle " + area);
+
+    }
+}
